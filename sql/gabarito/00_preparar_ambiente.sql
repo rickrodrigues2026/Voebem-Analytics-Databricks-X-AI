0623c427e3cf020e2433ebb2fffd0cccda8414a0
rickrodrigues2026 <ricardos.rodrigues@gmail.com>
2026-09-21 22:37:38 -0300

    Delete sql/gabarito/00_preparar_ambiente.sql

--- sql/gabarito/00_preparar_ambiente.sql
+++
@@ -1,6 +0,0 @@
--- Execute em um ambiente de estudos com permissao para criar estes objetos.
-CREATE CATALOG IF NOT EXISTS voebem;
-CREATE SCHEMA IF NOT EXISTS voebem.bronze;
-CREATE SCHEMA IF NOT EXISTS voebem.silver;
-CREATE SCHEMA IF NOT EXISTS voebem.gold;
-CREATE VOLUME IF NOT EXISTS voebem.bronze.arquivos;
